5d80376ee3629c98bfe8f57a64e8ccc354e31f83
Shahmeer Baig <157070187+ShahmeerBaig1@users.noreply.github.com>
2024-07-21 16:18:16 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,2 @@
-<<<<<<< HEAD
-=======
-
-#!/bin/bash
-
->>>>>>> baafe83d0b8d1df5fce58ec0088e9128253afb29
-# Build the React app using Vite
 npm run build
-
-# Install Python packages using pip
-<<<<<<< HEAD
 pip install -r backend/requirements.txt
-=======
-pip install -r backend/requirements.txt
->>>>>>> baafe83d0b8d1df5fce58ec0088e9128253afb29
